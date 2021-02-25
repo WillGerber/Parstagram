d86d10d50c531f0cf42cee5ea6cae11438daee2a
William Gerber <willgerber01@gmail.com>
2021-02-25 08:33:07 -0800

    Added authentication on login features

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "nIxUsDF9nXscYE5txtXt3Y53Os35tkpkluZXH1j4"
+            $0.clientKey = "FPOKfm27H9C9lT1668qFX457wdsGpMuhRQ2bmyCM"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
